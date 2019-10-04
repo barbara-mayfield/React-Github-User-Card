c501c383b208d9e27defbc9bc21e8d764a4c5e75
Barbara Mayfield <bmayfield1991@gmail.com>
2019-10-03 21:02:30 -0500

    Installed npm in the wrong folder, everything is working as intended now

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-
+class App extends Component {
+  render () {
+    return (
+      <>
+      <h1>Github User Cards</h1>
+      </>
+    )
+  }
+}
 
 export default App;
